7308ff3fa940ac6b0a747bdedeb97e106b94e7d9
Rahma AIT MAHFOUD <155021211+AitMahfoud-Rahma@users.noreply.github.com>
2024-02-21 16:46:58 +0100

    Update qc_mapping.R

--- QC_mapping/qc_mapping.R
+++ QC_mapping/qc_mapping.R
@@ -16,13 +16,19 @@ csvfile <- dplyr::mutate(csvfile, sample = strsplit(sample_type, split="-unmappe
 csvfile$sample <- as.character(csvfile$sample)
 csvfile$type <- as.factor(csvfile$type)
 
+# Calculate % of mapped/unmapped reads 
+csvfile <- csvfile %>%
+  group_by(sample) %>%
+  mutate(total_reads = sum(count),
+         percentage = count / total_reads * 100)
 # Create a supperposed plot for mapped and unmapped reads
-plot <- ggplot(csvfile, aes(fill=type, y=count, x=sample)) + 
+plot <- ggplot(csvfile, aes(fill=type, y=count, x=sample, label = paste0(round(percentage, 2), "%"))) + 
   geom_bar(width=0.95, position="stack", stat="identity") +
-  geom_text(aes(label = count), position = position_stack(vjust = 0.5), size = 3) +
+  geom_text(position = position_stack(vjust = 0.5), size = 3) +
   labs(fill="Type", x="Samples", y="Number of Reads") +
   ggtitle("Distribution of Mapped and Unmapped Reads") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 35, hjust = 1))+theme(plot.title = element_text(hjust = 0.5))
+
 # Save a plot on .pdf file
 ggsave("/groups/dog/stage/rahma/results/minimap2/analysis/QC/qc_mapping.pdf", plot, width=10, height=7)
