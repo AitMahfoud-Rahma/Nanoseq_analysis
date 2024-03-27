2a45ba172d8eb3da2931fd6c85d7bfa055441b5d
Rahma AIT MAHFOUD <155021211+AitMahfoud-Rahma@users.noreply.github.com>
2024-03-27 15:41:58 +0100

    Update short_v_plot.py

--- QC_variant_calling/Short_variants/short_v_plot.py
+++ QC_variant_calling/Short_variants/short_v_plot.py
@@ -3,34 +3,46 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 output_file = "short_variants_stat.txt"
+
+# Lecture du fichier de données
 samples = []
-num_variants = []
+num_variants_total = []
+num_variants_pass = []
 
 with open(output_file, 'r') as file:
-    next(file) 
+    next(file)  # Ignorer la première ligne contenant les en-têtes
     for line in file:
-        sample, variants = line.strip().split('\t')
+        sample, total, pass_variants = line.strip().split('\t')
         samples.append(sample)
-        num_variants.append(int(variants))
+        num_variants_total.append(int(total))
+        num_variants_pass.append(int(pass_variants))
 
-sample_colors = {}
-prefixes = set(sample.split('-')[0] for sample in samples) 
-num_prefixes = len(prefixes)
-colors = cm.tab10(range(num_prefixes)) 
-
-for i, prefix in enumerate(sorted(prefixes)):
-    for j in range(2): 
-        sample = f"{prefix}-{j+1}"
-        sample_colors[sample] = colors[i]
-
+# Création du graphique
 plt.figure(figsize=(10, 6))
-for sample, variants in zip(samples, num_variants):
-    color = sample_colors.get(sample, 'skyblue') 
-    plt.bar(sample, variants, color=color)
 
+# Création de l'indice pour les échantillons
+x = range(len(samples))
+
+# Largeur des barres
+bar_width = 0.35
+
+# Tracer les barres pour le nombre total de variants
+plt.bar(x, num_variants_total, bar_width, label='Total Variants')
+
+# Tracer les barres pour le nombre de variants PASS, en les superposant
+plt.bar([i + bar_width for i in x], num_variants_pass, bar_width, label='Variants PASS')
+
+# Ajout de titres et de labels
 plt.title('Number of variants per sample')
 plt.xlabel('Samples')
 plt.ylabel('Number of variants')
-plt.xticks(rotation=45, ha='right')
-plt.tight_layout() 
+
+# Rotation des étiquettes de l'axe x pour une meilleure lisibilité
+plt.xticks([i + bar_width / 2 for i in x], samples, rotation=45, ha='right')
+
+# Affichage de la légende
+plt.legend()
+
+# Affichage du graphique
+plt.tight_layout()  # Pour éviter que les étiquettes ne se chevauchent
 plt.savefig("short_v_stat.png")
