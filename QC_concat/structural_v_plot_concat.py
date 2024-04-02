e238a8087613469c94b61ef437f3ed846c713bc5
Rahma AIT MAHFOUD <155021211+AitMahfoud-Rahma@users.noreply.github.com>
2024-04-02 11:38:52 +0200

    Add files via upload

---
+++ QC_concat/structural_v_plot_concat.py
@@ -0,0 +1,30 @@
+import os
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+output_file = "structural_v_stat_concat.txt"
+
+samples = []
+num_variants_total = []
+num_variants_pass = []
+
+with open(output_file, 'r') as file:
+    next(file)  
+    for line in file:
+        sample, total, pass_variants = line.strip().split('\t')
+        samples.append(sample)
+        num_variants_total.append(int(total))
+        num_variants_pass.append(int(pass_variants))
+
+plt.figure(figsize=(10, 6))
+x = range(len(samples))
+bar_width = 0.35
+plt.bar(x, num_variants_total, bar_width, label='Total Variants')
+plt.bar([i + bar_width for i in x], num_variants_pass, bar_width, label='Variants PASS')
+plt.title('Number of variants per sample')
+plt.xlabel('Samples')
+plt.ylabel('Number of variants')
+plt.xticks([i + bar_width / 2 for i in x], samples, rotation=45, ha='right')
+plt.legend()
+plt.tight_layout() 
+plt.savefig("structural_v_stat_concat.png")
