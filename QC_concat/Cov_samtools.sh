2da47d4fb429066d1b4c78e2adc6ce975a3f8a5a
Rahma AIT MAHFOUD <155021211+AitMahfoud-Rahma@users.noreply.github.com>
2024-05-03 15:01:51 +0200

    Update Cov_samtools.sh

--- QC_concat/Cov_samtools.sh
+++ QC_concat/Cov_samtools.sh
@@ -6,6 +6,6 @@
 DIR="/groups/dog/stage/rahma/deepv_cutesv/results/minimap2/"
 
 for f in $DIR/*.bam; do
-    coverage=$(samtools coverage "$f" | awk 'NR > 1 {sum += $6; count++} END {if (count > 0) {percentage = (sum / count); printf "Coverage % : %.2f%%\n", percentage}}')
+    coverage=$(samtools coverage "$f" | awk 'NR > 1 {sum += ($6 * $3); count += $3} END {if (count > 0) {percentage = (sum / count); printf "Coverage %% : %.2f%%\n", percentage}}')
     echo "$(basename "$f") $coverage"
 done
