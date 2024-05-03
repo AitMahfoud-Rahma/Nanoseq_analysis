42b412968ff1d30e05aba2d164eade947326610c
Rahma AIT MAHFOUD <155021211+AitMahfoud-Rahma@users.noreply.github.com>
2024-05-03 09:37:28 +0200

    Add files via upload

---
+++ QC_concat/Cov_samtools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# AIT MAHFOUD Rahma 
+# Script to calculate the % of coverage using samtools coverage
+. /local/env/envsamtools-1.15.sh
+
+DIR="/groups/dog/stage/rahma/deepv_cutesv/results/minimap2/"
+
+for f in $DIR/*.bam; do
+    coverage=$(samtools coverage "$f" | awk 'NR > 1 {sum += $6; count++} END {if (count > 0) {percentage = (sum / count); printf "Coverage % : %.2f%%\n", percentage}}')
+    echo "$(basename "$f") $coverage"
+done
